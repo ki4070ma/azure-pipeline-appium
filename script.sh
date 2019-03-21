473c7e0aaea9f64179f537051150da58793531f0
Atsushi Mori <ki4070ma@gmail.com>
2019-03-22 02:29:25 +0900

    fixed adb path

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@
 $ANDROID_HOME/platform-tools/adb devices
 id=`docker ps -a | grep butomo | cut -d ' ' -f 1`
 ip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $id`
-adb connect $ip:5555
+$ANDROID_HOME/platform-tools/adb connect $ip:5555
 $ANDROID_HOME/platform-tools/adb devices
