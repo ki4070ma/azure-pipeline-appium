c4de1e85a98de44e474bf430cd9b7a9934de1cd9
Atsushi Mori <ki4070ma@gmail.com>
2019-03-22 02:43:35 +0900

    Some changes

--- script.sh
+++ script.sh
@@ -5,3 +5,7 @@ id=`docker ps -a | grep butomo | cut -d ' ' -f 1`
 ip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $id`
 $ANDROID_HOME/platform-tools/adb connect $ip:5555
 $ANDROID_HOME/platform-tools/adb devices
+lsof -n -i:4723 | grep LISTEN
+
+sleep 60
+$ANDROID_HOME/platform-tools/adb devices
